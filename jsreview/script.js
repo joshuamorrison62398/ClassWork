7336685ca9bdc3f1d5bcd95fe1eb150bca49af9e
joshua morrison <joshuamorrison623@gmail.com>
2022-08-18 15:16:21 -0400

    jsreview

---
+++ jsreview/script.js
@@ -0,0 +1,44 @@
+// Javascript Review
+  
+// 1. Variables and Datatypes
+  // A. Q + A
+    // 1. How do we assign a value to a variable
+    /* Answer: by using let or const to declare a varible and using equal sign to assign its value*/
+    
+    // 2. How do we change the value of a variable
+    /* Answer : redeclare varible in a different block */
+
+    // 3. how do we assign an existing variable to a new variable
+    /* Answer: declare varible without using let or const and use equal sign to reassign new varible */
+
+    //4. what are declare, assign and define
+    /* Answer: let x = 15 , let is declaring the variable, x is defining the varible, and assigned to the varible is 15 */
+    
+    //5. what is pseudocoding and why should you use it
+    /* Answer: simple way to describe a set of instructions, has own syntax. very similar to programming language.
+
+    //6. what percentage of time should be spent thinking about how you are going to solve a problem vs actually typing in code to solve it?
+    /* Answer: 90% */
+
+
+//2. Strings
+   //1. and 2.
+    let firstVariable = "Hello World!"
+   //3.
+    firstVariable = 200
+    //4.
+    let secondVariable = firstVariable;
+    //5.
+    secondVariable = "I am Batman!"
+    
+    //6.  200
+
+    //7. 
+    let yourName = 'joshua';
+
+    //8.
+    console.log(`Hello, my name is ${yourName}.`)
+
+
+
+
