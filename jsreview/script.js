3e8a83ffef5311fe78b7507e8449ca17e6cfb546
joshua morrison <joshuamorrison623@gmail.com>
2022-08-18 15:55:59 -0400

    jsreview

--- jsreview/script.js
+++ jsreview/script.js
@@ -84,7 +84,8 @@
  
 
 //6. Loops
- 
+  //basics
+
   for(let i = 0; i <= 10; i++) {
         console.log(i)
   }
@@ -97,7 +98,12 @@
     console.log(i)
   }
 
-  
+  //get even
+
+    for(let i = 0; i <= 100; i++){
+        if(i % 2 === 0)
+        console.log(i)
+    }
 
 
 
